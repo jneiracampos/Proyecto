5ae548b1326a50eaad624d2cff899887794c701d
storres21 <88690048+storres21@users.noreply.github.com>
2022-10-10 19:40:10 -0500

    hola

--- Entrega_1.py
+++ Entrega_1.py
@@ -2,6 +2,8 @@
 Esta funcion determina si una secuencia es subsecuencia de otra
 @param string1: secuencia 1
 @param string2: secuencia 2
+@param m: longitud de la secuencia 1 - y 
+@param n: longitud de la secuencia 2 - x
 @return: True si es subsecuencia, False si no lo es
 """
 def isSubSequence(string1, string2, m, n):
@@ -19,17 +21,42 @@ def isSubSequence(string1, string2, m, n):
     # If last characters are not matching
     return isSubSequence(string1, string2, m, n-1)
 
+
+
+def rightString2(string1, string2, primera, segunda):
+    if segunda == -1:
+        mayorPosicionLetra = string2.rfind(primera)
+        string2.replace(string2[mayorPosicionLetra-1],'A')
+    elif segunda != -1:
+        mayorPosicionLetra = string2.rfind(primera)
+        if mayorPosicionLetra != -1:
+            string2.replace(string2[mayorPosicionLetra-1],'A')
+        else:
+            string2.replace('A', primera)
+            mayorPosicionLetra = string2.rfind(segunda)
+            string2.replace(string2[mayorPosicionLetra-1],'B')
+    return string2
+
+
+
 """
 Esta funcion cuenta la cantidad de veces que se repite una subsecuencia en una secuencia
-@param string1: secuencia 1
-@param string2: secuencia 2
+@param string1: subsecuencia
+@param string2: secuencia
 @return: cantidad de veces que se repite la subsecuencia
 """
 def countSubSequence(string1, string2):
     m = len(string1)
     n = len(string2)
+    if m  == 1:
+        primera = string1
+        segunda = -1
+    elif m == 2:
+        primera = string1[0]
+        segunda = string1[1]
     if isSubSequence(string1, string2, m, n):
-        return 1 + countSubSequence(string1, string2[m:])
+        string2=rightString2(string1, string2, primera, segunda)
+        return 1 + countSubSequence(string1, string2)
     else:
         return 0
 
